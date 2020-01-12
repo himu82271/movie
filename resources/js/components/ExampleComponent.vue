8267ea794f99063c80ec7b96d3f5ecd8064255be
isahasan <45736204+isahasan@users.noreply.github.com>
2020-01-12 11:08:19 +0600

    proect working fine

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -55,32 +55,9 @@
     export default {
         data:()=>({
             show: false,
-            // list:[],
-            // tempList:[]
         }),
         props: {
             item: Object,
         }
-        // created() {
-        //     let vm = this;
-        //     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=e7fd9e644511491efdade2c455621de8&language=en-US&page=1')
-        //     .then(response => response.json())
-        //     .then(data =>{
-        //         vm.tempList= data.results;
-        //         vm.tempList.forEach(function(element){
-        //             var obj = {
-        //                 title:element.title,
-        //                 rating:element.vote_average,
-        //                 imageUrl:element.poster_path,
-        //             };
-        //             // if(obj.rating < 8.5)
-        //             // {
-        //             //     vm.list.push(obj)
-        //             // }
-        //             vm.list.push(obj)
-        //         });
-        //         console.log(vm.list);
-        //     })
-        // }
     }
 </script>
